7b0c16b9b154bb43188391b5b02f43c8eab33ce9
Rafael Franco <43421180+RafaelFC21@users.noreply.github.com>
2018-11-19 19:02:42 -0800

    E4-3 Franco Corona Rafael

---
+++ E4-3 Franco Corona Rafael/grafos/Grafo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafos
+{
+    
+    class Grafo
+    {
+        public void Desplegar()//Se crea metodo Desplegar
+        {
+            Console.WriteLine("Caminos posibles: " +
+                "\n1.- A, B, C, D, E, F, G" +
+                "\n2.- A, B, G, F, E, D, C" +
+                "\n3.- A, B, C, G, F, D, E" +
+                "\n4.- A, B, G, C, D, F, E" +
+                "\n5.- C, D, E, F, G, B, A" +
+                "\n6.- D, E, F, G, C, B, A" +
+                "\n7.- F, E, D, C, G, B, A" +
+                "\n8.- G, F, E, D, C, B, A" +
+                "\n9.- E, F, D, C, G, B, A" +
+                "\n10.- E, D, F, G, C, B, A");//Se Despliegan todas las rutas escritas manualmente...
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ E4-3 Franco Corona Rafael/grafos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grafo grafo = new Grafo();//Se crea objeto de la clase Grafo
+            grafo.Desplegar();//Se llama al metodo Desplegar de la clase Grafo
+        }
+    }
+}
